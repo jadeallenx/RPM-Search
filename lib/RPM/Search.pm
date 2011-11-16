360447b629ad45a750a4eb7c3821504b07955e30
Mark Allen <mrallen1@yahoo.com>
2011-11-16 14:36:25 -0600

    Initial boilerplate

---
+++ lib/RPM/Search.pm
@@ -0,0 +1,111 @@
+package RPM::Search;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+RPM::Search - The great new RPM::Search!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use RPM::Search;
+
+    my $foo = RPM::Search->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Mark Allen, C<< <mrallen1 at yahoo.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-rpm-search at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=RPM-Search>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc RPM::Search
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=RPM-Search>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/RPM-Search>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/RPM-Search>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/RPM-Search/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Mark Allen.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of RPM::Search
