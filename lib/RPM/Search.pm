9342e927069ec0c747f1274df2be999a928e179b
Mark Allen <mrallen1@yahoo.com>
2011-11-16 20:02:07 -0600

    Most docs added
    search() not fully documented yet.
    
    TODO:
    * Finish docs

--- lib/RPM/Search.pm
+++ lib/RPM/Search.pm
@@ -9,15 +9,74 @@ use Try::Tiny;
 
 our $VERSION = '0.01';
 
-try {
-    require DBD::SQLite;
-}
-catch {
-    die "This module requires DBD::SQLite. Try
-        yum -y install perl-DBD-SQLite";
-};
+=head1 NAME
+
+RPM::Search
+
+=head1 SYNOPSIS
+
+  # On (recent) RPM based systems
+  my $db = RPM::Search->new();
+
+  my $aref = $db->search(qr/perl-Mo\.se/);
+  # or
+  $aref = $db->search('perl-CGI-*');
+  # or
+  $aref = $db->search('cpanminus');
+
+  if ( $aref ) {
+    my $pkgs = join ", ", @{ $aref };
+    `/usr/bin/yum -y install $pkgs`;
+  }
+  else {
+      print "No matching packages\n";
+  }
+
+=head1 ATTRIBUTES
+
+=over
+
+=item *
+
+cache_base - base location of the yum data (default: none)
+
+=item *
+
+yum_primary_db - fully qualified path to the primary SQLite database 
+(default: none)
+
+=item *
+
+dbh - DBI handle to the yum SQLite database
+
+=back
+
+=head1 METHODS
+
+=over
+
+=item new()
+
+Make a new L<RPM::Search|RPM::Search> object.  Will automatically search 
+for an appropriate yum database and open a handle to the data set 
+unless you pass valid arguments to the F<dbh> and/or F<yum_primary_db>
+attributes at construction time.
+
+Returns a new L<RPM::Search|RPM::Search> object.
+
+=back
+
+=cut
 
 sub new {
+
+    try {
+        require DBD::SQLite;
+    }
+    catch {
+        die "This module requires DBD::SQLite. Try:\n\tsudo yum -y install perl-DBD-SQLite\n";
+    };
+
     my $class = shift;
     my $proto = ref $class || $class;
     
@@ -34,6 +93,7 @@ sub cache_base {
     my $param = shift;
 
     if ( $param ) {
+        return unless -d $param;
         $self->{'cache_base'} = $param;
     }
 
@@ -52,10 +112,38 @@ sub yum_primary_db {
     return $self->{'yum_primary_db'};
 }
 
+sub dbh {
+    my $self = shift;
+    my $param = shift;
+
+    if ( $param ) {
+        return unless ref($param) =~ /DBI/i;
+        $self->{'dbh'} = $param;
+    }
+
+    return $self->{'dbh'};
+}
+
+=over
+
+=item find_yum_db()
+
+This method searches for an appropriate yum database starting at the 
+location passed as a parameter. If no parameter is given, the method
+will use F<cache_base>. If F<cache_base> is not set, the method will 
+use F</var/cache/yum>.
+
+This call populates F<yum_primary_db>.
+
+The return value is boolean: true for success, false for failure.
+
+=back
+
+=cut
+
 sub find_yum_db {
     my $self = shift;
-
-    my $base = $self->cache_base() || "/var/cache/yum";
+    my $base = shift || $self->cache_base() || "/var/cache/yum";
 
 
     my $path;
@@ -70,43 +158,61 @@ sub find_yum_db {
         return 1;
     }
     else {
-        warn "Couldn't find any yum primary SQLite databases in $base";
+        warn "Couldn't find any yum primary SQLite databases in $base\n";
         return 0;
     }
 }
 
-sub dbh {
-    my $self = shift;
-    my $param = shift;
+=over
 
-    if ( $param ) {
-        return unless ref($param) =~ /DBI/i;
-        $self->{'dbh'} = $param;
-    }
+=item open_db()
 
-    return $self->{'dbh'};
-}
+This method opens a connection to the yum SQLite database.  The DSN
+is constructed from the passed in parameter.  If no parameter is
+passed in, the method will use F<yum_primary_db>.
+
+This method populates F<dbh>.
+
+This method causes a fatal error on any failure.
+
+=back
+
+=cut
 
 sub open_db {
     my $self = shift;
-
-    my $dsn = sprintf('dbi:SQLite:dbname=%s', $self->yum_primary_db);
+    my $dbname = shift || $self->yum_primary_db;
 
     try {
-        my $dbh = DBI->connect($dsn, undef, undef, {
+        my $dbh = DBI->connect("dbi:SQLite:dbname=$dbname", undef, undef, {
                         RaiseError => 1,
                     sqlite_unicode => 1, 
                 } ) or die $DBI::err;
         $self->dbh($dbh);
     }
     catch {
-        die "Couldn't open db " . $self->yum_primary_db . ": $_";
+        die "Couldn't open db $dbname: $_\n";
     };
 }
+
+=over
+
+=item search()
+
+This method searches the RPM database using a 
+
+=back
+
+=cut
 
 sub search {
     my $self = shift;
     my $pattern = shift;
+
+    unless ( $pattern ) {
+        warn "You must pass a pattern.\n";
+        return undef;
+    }
 
     my $sql = "SELECT name FROM packages WHERE name ";
 
@@ -123,7 +229,13 @@ sub search {
         $sql .= "=?";
     }
 
-    return $self->dbh->selectcol_arrayref($sql, undef, $pattern);
+    try {
+        return $self->dbh->selectcol_arrayref($sql, undef, $pattern) or die $DBI::err;
+    }
+    catch {
+        warn "Couldn't execute query $sql: $_\n";
+        return undef;
+    }
 }
 
 1;
