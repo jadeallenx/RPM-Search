e04ffc592feee9c340f4f8b512c75c3a92da1609
Mark Allen <mrallen1@yahoo.com>
2011-11-17 00:34:51 -0600

    Docs and tests done
    * Created test database
    * Created 9 basic tests
    * Finished documentation
    * Added prereqs to Makefile.PL
    * Refactored some code in Search.pm

--- lib/RPM/Search.pm
+++ lib/RPM/Search.pm
@@ -18,7 +18,7 @@ RPM::Search
   # On (recent) RPM based systems
   my $db = RPM::Search->new();
 
-  my $aref = $db->search(qr/perl-Mo\.se/);
+  my $aref = $db->search(qr/perl-Mo.se/);
   # or
   $aref = $db->search('perl-CGI-*');
   # or
@@ -32,24 +32,85 @@ RPM::Search
       print "No matching packages\n";
   }
 
+=head1 PURPOSE
+
+This module allows one to search the entire collection of RPMs available for 
+a given installed Linux distribution that uses RPMs, not just listing the RPMs 
+already installed on a system.
+
+And frankly, have you tried using C<yum search>? 
+
+Eventually, I plan to use this functionality to make a plugin for 
+C<cpanminus> to suggest vendor-supplied packages instead of building 
+them "from scratch."
+
 =head1 ATTRIBUTES
 
 =over
 
-=item *
+=item cache_base 
 
-cache_base - base location of the yum data (default: none)
-
-=item *
-
-yum_primary_db - fully qualified path to the primary SQLite database 
-(default: none)
-
-=item *
-
-dbh - DBI handle to the yum SQLite database
+Base location of the yum data (default: none)
 
 =back
+
+=cut
+
+sub cache_base {
+    my $self = shift;
+    my $param = shift;
+
+    if ( $param ) {
+        return unless -d $param;
+        $self->{'cache_base'} = $param;
+    }
+
+    return $self->{'cache_base'};
+}
+
+=over 
+
+=item yum_primary_db 
+
+Fully qualified path to the primary SQLite database (default: none)
+
+=back
+
+=cut 
+
+sub yum_primary_db {
+    my $self = shift;
+    my $param = shift;
+
+    if ( $param ) {
+        return unless -e $param;
+        $self->{'yum_primary_db'} = $param;
+    }
+
+    return $self->{'yum_primary_db'};
+}
+
+=over
+
+=item dbh 
+
+DBI handle to the yum SQLite database
+
+=back
+
+=cut
+
+sub dbh {
+    my $self = shift;
+    my $param = shift;
+
+    if ( $param ) {
+        return unless ref($param) =~ /DBI/i;
+        $self->{'dbh'} = $param;
+    }
+
+    return $self->{'dbh'};
+}
 
 =head1 METHODS
 
@@ -88,41 +149,8 @@ sub new {
     return $self;
 }
 
-sub cache_base {
-    my $self = shift;
-    my $param = shift;
 
-    if ( $param ) {
-        return unless -d $param;
-        $self->{'cache_base'} = $param;
-    }
 
-    return $self->{'cache_base'};
-}
-
-sub yum_primary_db {
-    my $self = shift;
-    my $param = shift;
-
-    if ( $param ) {
-        return unless -e $param;
-        $self->{'yum_primary_db'} = $param;
-    }
-
-    return $self->{'yum_primary_db'};
-}
-
-sub dbh {
-    my $self = shift;
-    my $param = shift;
-
-    if ( $param ) {
-        return unless ref($param) =~ /DBI/i;
-        $self->{'dbh'} = $param;
-    }
-
-    return $self->{'dbh'};
-}
 
 =over
 
@@ -147,10 +175,13 @@ sub find_yum_db {
 
 
     my $path;
-    find( sub {
-            $path = $File::Find::name if /primary\.sqlite\z/ &&
+    find( { wanted => 
+            sub {
+                $path = $File::Find::name if /primary\.sqlite\z/ &&
                 $File::Find::dir !~ /update/ 
-          }, 
+            }, 
+            untaint => 1, 
+    },
     $base);
 
     if ( $path ) {
@@ -199,7 +230,35 @@ sub open_db {
 
 =item search()
 
-This method searches the RPM database using a 
+This method searches the RPM database using an optional pattern parameter.
+If no pattern is given, the method returns all available package names. 
+(B<Note>: This will be thousands of packages.)
+
+The format of the pattern can be one of the following:
+
+=over
+
+=item *
+
+A regular expression using the C<qr//> construct.
+
+=item *
+
+A SQL-ish wildcard expression using % and _
+
+=item *
+
+A filesystem like glob expression using ? and *
+
+=item *
+
+A scalar which must be a literal match for a package name in 
+the database to return any results
+
+=back
+
+The method returns an arrayref of all matching package names
+(which may be zero results.)  Undef is returned on errors.
 
 =back
 
@@ -209,33 +268,90 @@ sub search {
     my $self = shift;
     my $pattern = shift;
 
-    unless ( $pattern ) {
-        warn "You must pass a pattern.\n";
-        return undef;
-    }
+    my $sql = "SELECT name FROM packages";
+    my @bind_params;
 
-    my $sql = "SELECT name FROM packages WHERE name ";
+    if ( $pattern ) {
+        $sql .= " WHERE name ";
 
-    if ( ref($pattern) =~ /regexp/i ) {
-        $sql .= "REGEXP ?";
-    }
-    elsif ( $pattern =~ /[%_]/ ) {
-        $sql .= "LIKE ?";
-    }
-    elsif ( $pattern =~ /[\?\*]/ ) {
-        $sql .= "GLOB ?";
-    }
-    else {
-        $sql .= "=?";
+        if ( ref($pattern) =~ /regexp/i ) {
+            $sql .= "REGEXP ?";
+        }
+        elsif ( $pattern =~ /[%_]/ ) {
+            $sql .= "LIKE ?";
+        }
+        elsif ( $pattern =~ /[\?\*]/ ) {
+            $sql .= "GLOB ?";
+        }
+        else {
+            $sql .= "=?";
+        }
+        push @bind_params, $pattern;
     }
 
     try {
-        return $self->dbh->selectcol_arrayref($sql, undef, $pattern) or die $DBI::err;
+        return $self->dbh->selectcol_arrayref($sql, undef, @bind_params) or die $DBI::err;
     }
     catch {
         warn "Couldn't execute query $sql: $_\n";
         return undef;
     }
 }
+
+=head1 AUTHOR
+
+Mark Allen, C<mrallen1 at yahoo dot com>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-rpm-search at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=RPM-Search>. 
+
+I will be notified, and then you'll automatically be notified of progress on your 
+bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+perldoc RPM::Search
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=RPM-Search>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/RPM-Search>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/RPM-Search>
+
+=item * MetaCPAN
+
+L<https://metacpan.org/dist/RPM-Search/>
+
+=item * Github
+
+L<https://github.com/mrallen1/RPM-Search.git>
+
+=back
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Mark Allen.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
 
 1;
